5001f4219692c3b2a9c309c4ce78500f8105a5a1
jose luiz santos silva <59897471+joseluizDev@users.noreply.github.com>
2024-12-08 10:56:47 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
-
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
-
 from selenium.common.exceptions import WebDriverException, NoSuchDriverException
-
+from dotenv import load_dotenv
 import time
 import requests
 import os
@@ -13,38 +11,27 @@ import re
 import base64
 from flask import Flask
 import hashlib
-import sys
+
+# Carregar as variáveis do .env
+load_dotenv()
 
 extensionId = 'ilehaonighjijnmpnagapkhpcdbhclfg'
 CRX_URL = "https://clients2.google.com/service/update2/crx?response=redirect&prodversion=98.0.4758.102&acceptformat=crx2,crx3&x=id%3D~~~~%26uc&nacl_arch=x86-64"
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
 
-try:
-    USER = os.environ['GRASS_USER']
-    PASSW = os.environ['GRASS_PASS']
-except:
-    USER = ''
-    PASSW = ''
+USER = os.getenv('GRASS_USER', '')
+PASSW = os.getenv('GRASS_PASS', '')
+ALLOW_DEBUG = os.getenv('ALLOW_DEBUG', 'False').lower() == 'true'
 
-try:
-    ALLOW_DEBUG = os.environ['ALLOW_DEBUG']
-    if ALLOW_DEBUG == 'True':
-        ALLOW_DEBUG = True
-    else:
-        ALLOW_DEBUG = False
-except:
-    ALLOW_DEBUG = False
-
-# are they set?
+# Verificar se as variáveis estão configuradas
 if USER == '' or PASSW == '':
-    print('Please set GRASS_USER and GRASS_PASS env variables')
+    print('Por favor, configure GRASS_USER e GRASS_PASS no arquivo .env')
     exit()
 
-if ALLOW_DEBUG == True:
+if ALLOW_DEBUG:
     print('Debugging is enabled! This will generate a screenshot and console logs on error!')
 
-
-#https://gist.github.com/ckuethe/fb6d972ecfc590c2f9b8
+# Funções auxiliares
 def download_extension(extension_id):
     url = CRX_URL.replace("~~~~", extension_id)
     headers = {"User-Agent": USER_AGENT}
@@ -52,19 +39,15 @@ def download_extension(extension_id):
     with open("grass.crx", "wb") as fd:
         for chunk in r.iter_content(chunk_size=128):
             fd.write(chunk)
-    if ALLOW_DEBUG == True:
-        #generate md5 of file
+    if ALLOW_DEBUG:
         md5 = hashlib.md5(open('grass.crx', 'rb').read()).hexdigest()
         print('Extension MD5: ' + md5)
 
 
-
 def generate_error_report(driver):
-    if ALLOW_DEBUG == False:
+    if not ALLOW_DEBUG:
         return
-    #grab screenshot
     driver.save_screenshot('error.png')
-    #grab console logs
     logs = driver.get_log('browser')
     with open('error.log', 'w') as f:
         for log in logs:
@@ -77,32 +60,30 @@ def generate_error_report(driver):
     print(response.text)
     print('Error report generated! Provide the above information to the developer for debugging purposes.')
 
+# Processo principal
 print('Downloading extension...')
 download_extension(extensionId)
 print('Downloaded! Installing extension and driver manager...')
 
 options = webdriver.ChromeOptions()
-#options.binary_location = '/usr/bin/chromium-browser'
 options.add_argument("--headless=new")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument('--no-sandbox')
-
 options.add_extension('grass.crx')
 
 print('Installed! Starting...')
 try:
     driver = webdriver.Chrome(options=options)
-except (WebDriverException, NoSuchDriverException) as e:
+except (WebDriverException, NoSuchDriverException):
     print('Could not start with Manager! Trying to default to manual path...')
     try:
         driver_path = "/usr/bin/chromedriver"
         service = ChromeService(executable_path=driver_path)
         driver = webdriver.Chrome(service=service, options=options)
-    except (WebDriverException, NoSuchDriverException) as e:
+    except (WebDriverException, NoSuchDriverException):
         print('Could not start with manual path! Exiting...')
         exit()
 
-#driver.get('chrome-extension://'+extensionId+'/index.html')
 print('Started! Logging in...')
 driver.get('https://app.getgrass.io/')
 
@@ -123,18 +104,13 @@ while True:
             driver.quit()
             exit()
 
-#find name="user"
 user = driver.find_element('xpath', '//*[@name="user"]')
 passw = driver.find_element('xpath', '//*[@name="password"]')
 submit = driver.find_element('xpath', '//*[@type="submit"]')
 
-#get user from env
 user.send_keys(USER)
 passw.send_keys(PASSW)
 submit.click()
-
-#id="chakra-toast-manager-top-right" is the toast
-
 
 sleep = 0
 while True:
@@ -169,7 +145,6 @@ while True:
             exit()
 
 print('Connected! Starting API...')
-#flask api
 app = Flask(__name__)
 
 @app.route('/')
@@ -186,21 +161,14 @@ def get():
         token = driver.find_element('xpath', '//*[@alt="token"]')
         token = token.find_element('xpath', 'following-sibling::div')
         epoch_earnings = token.text
-    except Exception as e:
+    except:
         epoch_earnings = False
         print('Could not get earnings!')
         generate_error_report(driver)
     
     try:
-        #find all chakra-badge
         badges = driver.find_elements('xpath', '//*[contains(@class, "chakra-badge")]')
-        #find the one with chakra-text that contains either "Connected" or "Disconnected"
-        connected = False
-        for badge in badges:
-            text = badge.find_element('xpath', 'child::div//p').text
-            if 'Connected' in text:
-                connected = True
-                break
+        connected = any('Connected' in badge.text for badge in badges)
     except:
         connected = False
         print('Could not get connection status!')
@@ -208,6 +176,5 @@ def get():
 
     return {'connected': connected, 'network_quality': network_quality, 'epoch_earnings': epoch_earnings}
 
-
-app.run(host='0.0.0.0',port=80, debug=False)
+app.run(host='0.0.0.0', port=80, debug=False)
 driver.quit()
